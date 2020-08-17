71aa65f7dfb21deca9071c96588175dd60916a64
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-08-17 17:41:29 +0300

    Initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+// Bring in local connection string (ignored in Git)
+const MONGO_STRING = require("./keys").MONGO_STRING;
+
+module.exports = {
+  ENV: process.env.NODE_ENV || "development",
+  PORT: process.env.PORT || 3000,
+  URL: process.env.BASE_URL || "http://localhost:3000",
+  MONGODB_URI: process.env.MONGODB_URI || MONGO_STRING,
+};
